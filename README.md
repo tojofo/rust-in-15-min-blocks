8d2e0b7ae45dd91b215235c151cba31f808f6811
Tommy Fotak <tommy@biteable.com>
2022-09-30 07:38:48 +1000

    Hello world example

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # rust-in-15-min-blocks
 Me, learning Rust in 15 minute blocks
+
+## 2022-09-30
+Hello world
---
+++ hello_world.rs
@@ -0,0 +1,4 @@
+fn main() {
+    println!("Hello World!");
+    println!("I _AM_ learning Rust (finally after 10 years)");
+}
